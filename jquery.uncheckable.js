4034def813830f1c1896f58d167bd2732ce85528
Renaat De Muynck <renaat.demuynck@gmail.com>
2015-10-29 16:02:26 +0100

    Add support for labels with FOR attribute

--- jquery.uncheckable.js
+++ jquery.uncheckable.js
@@ -5,19 +5,20 @@
  */
 
 $.fn.uncheckable = function () {
+    
+    'use strict';
+    
     this.each(function () {
-        
-        'use strict';
-        
         var radio = $(this),
-            label = radio.closest('label').length === 1 ? radio.closest('label') : radio;
+            parentLabel = radio.closest('label'),
+            targets = $('label[for="' + this.id + '"]').add(parentLabel.length === 1 ? parentLabel : radio);
         
-        // Capture MOUSEUP event on radio button or its label
-        // At this point we know the current state of the radio button
-        label.mouseup(function (e) {
-            
-            // Attach a one-time click handler to the radio button
-            // *only* if the state is 'checked' and the left mouse button was used
+        // Capture MOUSEUP event on radio button (or its parent label)
+        // and all labels with a FOR attribute pointing to it
+        targets.mouseup(function (e) {
+            // At this point we know the current state of the radio button
+            // so if the state is 'checked' and the left mouse button was used
+            // we attach a one-time click handler to the radio button
             if (radio.prop('checked') && e.which === 1) {
                 radio.one('click', function () {
                     radio.prop('checked', false);
@@ -25,4 +26,5 @@ $.fn.uncheckable = function () {
             }
         });
     });
+    
 };

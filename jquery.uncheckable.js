05fc6d1f00adc8d0d0d9ba7eb84e6e3b4378233d
Renaat De Muynck <renaat.demuynck@gmail.com>
2015-11-05 15:11:07 +0100

    Put all code inside an Immediately Invoked Function Expression

--- jquery.uncheckable.js
+++ jquery.uncheckable.js
@@ -4,27 +4,29 @@
  * @license MIT
  */
 
-$.fn.uncheckable = function () {
-    
-    'use strict';
-    
-    this.each(function () {
-        var radio = $(this),
-            parentLabel = radio.closest('label'),
-            targets = $('label[for="' + this.id + '"]').add(parentLabel.length === 1 ? parentLabel : radio);
+(function ($) {
+    $.fn.uncheckable = function () {
         
-        // Capture MOUSEUP event on radio button (or its parent label)
-        // and all labels with a FOR attribute pointing to it
-        targets.mouseup(function (e) {
-            // At this point we know the current state of the radio button
-            // so if the state is 'checked' and the left mouse button was used
-            // we attach a one-time click handler to the radio button
-            if (radio.prop('checked') && e.which === 1) {
-                radio.one('click', function () {
-                    radio.prop('checked', false);
-                });
-            }
+        'use strict';
+        
+        this.each(function () {
+            var radio = $(this),
+                parentLabel = radio.closest('label'),
+                targets = $('label[for="' + this.id + '"]').add(parentLabel.length === 1 ? parentLabel : radio);
+            
+            // Capture MOUSEUP event on radio button (or its parent label)
+            // and all labels with a FOR attribute pointing to it
+            targets.mouseup(function (e) {
+                // At this point we know the current state of the radio button
+                // so if the state is 'checked' and the left mouse button was used
+                // we attach a one-time click handler to the radio button
+                if (radio.prop('checked') && e.which === 1) {
+                    radio.one('click', function () {
+                        radio.prop('checked', false);
+                    });
+                }
+            });
         });
-    });
-    
-};
+        
+    };
+}(jQuery));
